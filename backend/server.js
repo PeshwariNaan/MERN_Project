fbb8cd66c2556c7fdd70ab76528568fe6a4b084f
Your Name <you@eaxample.com>
2021-01-31 13:08:28 -0700

    Added mongodb and changed the env files - changed all the server.js and changed the package.json to use import instead of require

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,14 @@
-const express = require('express'); 
-const products = require('./data/products.js')
+import express from 'express'; 
+import products from './data/products.js'; //don't forget the .js
+import connectDB from './config/db.js';
+import dotenv from 'dotenv';
+
+dotenv.config()
+
+connectDB();
 
 const app = express();
+
 
 //Lesson 11 - We added express and the backend folder and we added the start script in the root package.json file so we can use $npm start from the cmd terminal
 
@@ -18,6 +25,6 @@ app.get('/api/products/:id', (req, res) => {
     res.json(product)
 });
 
+const PORT = process.env.PORT || 5000
 
-
-app.listen(5000, console.log('Server running on port 5000'));
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
